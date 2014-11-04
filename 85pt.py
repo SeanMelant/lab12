61ec48ee914c08127e6a364b3cd10098c7ccfb43
SeanMelant <colgatetoothpasteyum@gmail.com>
2014-11-04 14:01:49 -0600

    85
    lkkllklk

--- 85pt.py
+++ 85pt.py
@@ -4,33 +4,31 @@
 #
 #########################################
 
-
-# Add a cancel button with a red background
-# When the cancel button is pressed, change the color from red to blue
-# and then back to red when pressed again
-# Read the comment above line 24
 from Tkinter import *
 
 class MyApp:
 	def __init__(self, parent):
-		self.myParent = parent  ### (7) remember my parent, the root
+		self.myParent = parent
 		self.myContainer1 = Frame(parent)
 		self.myContainer1.pack()
-		
 		self.button1 = Button(self.myContainer1)
 		self.button1.configure(text="OK", background= "green")
-		self.button1.grid(row=0, column=0)	
-		# Do not change <Button-1> when you create Button 2 :)
-		self.button1.bind("<Button-1>", self.button1Click) ### (1)
-		
-		
-	def button1Click(self, event):    ### (3)
-		if self.button1["background"] == "green": ### (4)
+		self.button1.pack(side=LEFT)	
+		self.button1.bind("<Button-1>", self.button1Click)
+		self.button2 = Button(self.myContainer1)
+		self.button2.configure(text="Cancel", background= "red")
+		self.button2.pack(side=RIGHT)	
+		self.button2.bind("<Button-2>", self.button2Click) 
+	def button1Click(self, event): 
+		if self.button1["background"] == "green":
 			self.button1["background"] = "yellow"
 		else:
 			self.button1["background"] = "green"
-	
-		
+	def button2Click(self, event): 
+		if self.button2["background"] == "red": 
+			self.button2["background"] = "blue"
+		else:
+			self.button2["background"] = "red"
 root = Tk()
 myapp = MyApp(root)
 root.mainloop()
