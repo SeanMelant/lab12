1581c6bf74d93f176ff9fe63b48fc30348cf21de
SeanMelant <colgatetoothpasteyum@gmail.com>
2014-11-04 13:22:47 -0600

    75
    ff

--- 70pt.py
+++ 70pt.py
@@ -25,9 +25,12 @@ class MyApp:
 		
 
 		self.button3 = Button(self.myContainer1)
-		self.button3.configure(text="Test?", background="cyan")  
+		self.button3.configure(text="moosic", background="cyan")  
 		self.button3.grid(row=0,column=2)	  ### (3)
 			
+		self.button4 = Button(self.myContainer1)
+		self.button4.configure(text="poopy", background="blue")  
+		self.button4.grid(row=0,column=3)	  ### (4)
 	
 		
 root = Tk()
